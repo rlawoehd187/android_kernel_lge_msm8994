eb86b3267e9d0be5b711b293178a058721754263
yangdongdong <yangdongdong@xiaomi.com>
2015-08-08 11:59:59 +0800

    power: align wakeup_sources format
    This aligns every column of elements in wakeup_sources to
    conveniently check any specific column for suspicious power
    consumption wakeup source or for other easily human readable purpose.
    
    Change-Id: I69137fc4af272f392f3e0c1a4bd48d5a5dee7d2c
    Signed-off-by: yangdongdong <yangdongdong@xiaomi.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -961,7 +961,7 @@ static int print_wakeup_source_stats(struct seq_file *m,
 		active_time = ktime_set(0, 0);
 	}
 #ifdef CONFIG_LGE_PM_DEBUG
-	ret = seq_printf(m, "%-12s\t%lu\t\t%lu\t\t%lu\t\t%lu\t\t%lu\t\t"
+	ret = seq_printf(m, "%-32s\t%lu\t\t%lu\t\t%lu\t\t%lu\t\t%lu\t\t"
 			"%lld\t\t%lld\t\t%lld\t\t%lld\t\t%lld\n",
 			ws->name, active_count, ws->event_count,
 			ws->wakeup_count, ws->expire_count, ws->pending_count,
@@ -969,7 +969,7 @@ static int print_wakeup_source_stats(struct seq_file *m,
 			ktime_to_ms(max_time), ktime_to_ms(ws->last_time),
 			ktime_to_ms(prevent_sleep_time));
 #else
-	ret = seq_printf(m, "%-12s\t%lu\t\t%lu\t\t%lu\t\t%lu\t\t"
+	ret = seq_printf(m, "%-32s\t%lu\t\t%lu\t\t%lu\t\t%lu\t\t"
 			"%lld\t\t%lld\t\t%lld\t\t%lld\t\t%lld\n",
 			ws->name, active_count, ws->event_count,
 			ws->wakeup_count, ws->expire_count,
@@ -1040,11 +1040,11 @@ static int wakeup_sources_stats_show(struct seq_file *m, void *unused)
 	struct wakeup_source *ws;
 
 #ifdef CONFIG_LGE_PM_DEBUG
-	seq_puts(m, "name\t\tactive_count\tevent_count\twakeup_count\t"
+	seq_puts(m, "name\t\t\t\t\tactive_count\tevent_count\twakeup_count\t"
 			"expire_count\tpending_count\tactive_since\ttotal_time\t"
 			"max_time\tlast_change\tprevent_suspend_time\n");
 #else
-	seq_puts(m, "name\t\tactive_count\tevent_count\twakeup_count\t"
+	seq_puts(m, "name\t\t\t\t\tactive_count\tevent_count\twakeup_count\t"
 		"expire_count\tactive_since\ttotal_time\tmax_time\t"
 		"last_change\tprevent_suspend_time\n");
 #endif
