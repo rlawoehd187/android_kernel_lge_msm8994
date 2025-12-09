39c6da15bd211ba1f198d746df727934933fb467
hiru <rlawoehd187@naver.com>
2025-12-09 11:19:51 +0900

    msm: camera: sensor: Fix memory leaks when sensor probe fails
    The power settings for a camera sensor can be parsed and have its memory
    never freed in the error path if part of the probe sequence fails after
    the power settings are parsed.
    
    Free the power settings in the error paths to fix the memory leaks.
    
    * fix this commit 9bfb3d780f39fc7a6227472e541462598300a9e1
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -1012,6 +1012,9 @@ camera_power_down:
 	s_ctrl->func_tbl->sensor_power_down(s_ctrl);
 free_camera_info:
 	kfree(camera_info);
+free_power_settings:
+	kfree(s_ctrl->sensordata->power_info.power_setting);
+	kfree(s_ctrl->sensordata->power_info.power_down_setting);
 free_slave_info:
 	kfree(slave_info);
 	return rc;
